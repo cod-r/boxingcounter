dbd572b553584c17dac95fae0ea3895143eef465
codR <panea_codrut@yahoo.com>
2017-12-19 17:39:56 +0200

    Project finished

--- app/src/main/java/com/example/panea/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/panea/courtcounter/MainActivity.java
@@ -2,12 +2,90 @@ package com.example.panea.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+
+    public void displayForTeamA(int score) {
+
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+
+        scoreView.setText(String.valueOf(score));
+
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+
+     * Updates the score for team A.
+
+     */
+
+    public void addThreeToTeamA (View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoToTeamA (View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+
+    public void addOneToTeamA (View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+
+     * Updates the score for team B.
+
+     */
+
+    public void addThreeToTeamB (View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoToTeamB (View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneToTeamB (View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Resets score for both teams
+     */
+
+    public void resetScore (View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
